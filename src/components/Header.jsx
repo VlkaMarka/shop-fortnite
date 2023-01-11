a455ac2ddba1c1695404887a8bc5dcc436d3958d
VlkaMarka <edx860@gmail.com>
2023-01-11 20:53:42 +0300

    gh-pages added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,18 @@ function Header() {
   return (
     <nav className="#1565c0 blue darken-3">
       <div className="nav-wrapper">
-        <a href="!#" className="left brand-logo">
+        <a href="/" className="left brand-logo">
           Shop Fortnite
         </a>
         <ul id="nav-mobile" className="right">
           <li>
-            <a href="!#">Repo</a>
+            <a
+              href="https://github.com/VlkaMarka/shop-fortnite"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Repo
+            </a>
           </li>
         </ul>
       </div>
